d29ce3ba6d487850618942b7656ecf2186cad64e
Pieter Verschaffelt <pieter.verschaffelt@ugent.be>
2019-08-30 14:38:02 +0200

    Trying to get external library to work

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <SearchSettingsForm/>
   </div>
 </template>
 
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
 import Vue from 'vue';
 import App from './App.vue';
+import * as unipept from 'unipept-web-components';
+
+console.log(unipept);
+
+Vue.use(unipept);
 
 Vue.config.productionTip = false;
 
