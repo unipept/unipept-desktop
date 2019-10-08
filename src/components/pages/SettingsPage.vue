b0faec2e01796770967c80df8e7f3e4e29d9e4fc
Pieter Verschaffelt <pieter.verschaffelt@ugent.be>
2019-10-08 16:38:01 +0200

    Forget to declare SettingsPage as Component

--- src/components/pages/SettingsPage.vue
+++ src/components/pages/SettingsPage.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
+
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
+import Component from 'vue-class-component';
 
+@Component
 export default class SettingsPage extends Vue {
 
 }
 </script>
 
-<style lang="less">
+<style>
 
 </style>
