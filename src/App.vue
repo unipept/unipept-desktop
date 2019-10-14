372454b5d9e58f5c2f400a865015eb502f8f86a1
Pieter Verschaffelt <pieter.verschaffelt@ugent.be>
2019-10-14 11:55:58 +0200

    Fix chevron

--- src/App.vue
+++ src/App.vue
@@ -40,9 +40,9 @@
 
       <!-- Navigation drawer for managing the currently selected peptides / experiments / etc. Is positioned on the 
            right side -->
-      <Toolbar :open.sync="rightNavDrawer" :mini.sync="rightNavMini" v-on:activate-dataset="onActivateDataset"></Toolbar>
+      <Toolbar :open.sync="rightNavDrawer" v-on:activate-dataset="onActivateDataset"></Toolbar>
 
-      <v-content style="min-height: 100%; max-width: calc(100% - 80px);" :class="{'open-right-nav-drawer': !rightNavMini}">
+      <v-content style="min-height: 100%; max-width: calc(100% - 80px); position: relative; left: 80px;" :class="{'open-right-nav-drawer': !rightNavMini}">
         <router-view style="min-height: 100%;"></router-view>
       </v-content>
     </v-app>
@@ -55,16 +55,17 @@ import Component from 'vue-class-component';
 import { Prop, Watch } from 'vue-property-decorator';
 import PeptideContainer from 'unipept-web-components/src/logic/data-management/PeptideContainer';
 import Toolbar from './components/navigation-drawers/Toolbar.vue';
+import SampleManager from './components/sample/SampleManager.vue';
 
 @Component({
   components: {
-    Toolbar
+    Toolbar,
+    SampleManager
   }
 })
 export default class App extends Vue {
   private navDrawer: boolean = false;
   private rightNavDrawer: boolean = true;
-  private rightNavMini: boolean = true;
 
   private selectDataset(value: PeptideContainer) {
     // @ts-ignore
@@ -97,9 +98,5 @@ export default class App extends Vue {
 
   .nav-drawer .v-divider {
     margin-top: 7px !important;
-  }
-
-  .open-right-nav-drawer {
-    max-width: calc(100% - 256px) !important;
   }
 </style>
--- src/components/navigation-drawers/Toolbar.vue
+++ src/components/navigation-drawers/Toolbar.vue
@@ -1,76 +1,99 @@
 <template>
-  <v-navigation-drawer v-model="isOpen" :mini-variant.sync="isMini" fixed right>
-    <v-list style="position: relative; top: 64px;">
-        <v-list-group prepend-icon="mdi-format-list-bulleted">
-            <template v-slot:activator>
-                <v-list-item-content>
-                <v-list-item-title>Samples</v-list-item-title>
-                </v-list-item-content>
-            </template>
-            
-            <v-list-item @click="activateDataset(dataset)" v-for="dataset of this.$store.getters.selectedDatasets" :key="dataset.id">
-                <v-list-item-title>
-                {{ dataset.getName() }}
-                </v-list-item-title>
-                <v-list-item-subtitle>
-                {{ dataset.getAmountOfPeptides() }} peptides
-                </v-list-item-subtitle>
-                <v-list-item-action v-if="dataset.progress !== 1">
-                    <v-progress-circular :rotate="-90" :size="24" :value="dataset.progress * 100" color="primary"></v-progress-circular>
-                </v-list-item-action>
-            </v-list-item>
+    <div>
+        <v-navigation-drawer v-model="isOpen" :mini-variant="true" fixed :class="{'toolbar-navigation-drawer': !isMini}">
+            <div style="position: relative; top: 64px;">
+                <v-list>
+                    <v-list-item link to='/'>
+                        <v-list-item-icon>
+                            <v-icon>mdi-bacteria</v-icon>
+                        </v-list-item-icon>
+                        <v-list-item-content>
+                            <v-list-item-title>Analyse</v-list-item-title>
+                        </v-list-item-content>
+                    </v-list-item>
+                    <v-list-group prepend-icon="mdi-bacteria" >
+                        <template v-slot:activator>
+                            <v-list-item-content to="/">
+                                <v-list-item-title>Analyse</v-list-item-title>
+                            </v-list-item-content>
+                        </template>
+                        
+                        <v-list-item @click="activateDataset(dataset)" v-for="dataset of this.$store.getters.selectedDatasets" :key="dataset.id">
+                            <v-list-item-title>
+                            {{ dataset.getName() }}
+                            </v-list-item-title>
+                            <v-list-item-subtitle>
+                            {{ dataset.getAmountOfPeptides() }} peptides
+                            </v-list-item-subtitle>
+                            <v-list-item-action v-if="dataset.progress !== 1">
+                                <v-progress-circular :rotate="-90" :size="24" :value="dataset.progress * 100" color="primary"></v-progress-circular>
+                            </v-list-item-action>
+                        </v-list-item>
 
-            <div>
-                <v-btn @click="selectSample" class="select-sample-button" depressed color="primary">Select sample</v-btn>
+                        <div>
+                            <v-btn @click="selectSample" class="select-sample-button" depressed color="primary">Select sample</v-btn>
+                        </div>
+                    </v-list-group>
+
+                    <v-list-group prepend-icon="mdi-tune">
+                        <template v-slot:activator>
+                            <v-list-item-content>
+                            <v-list-item-title>Search settings</v-list-item-title>
+                            </v-list-item-content>
+                        </template>
+
+                        <v-list-item>
+                            <template v-slot:default="{active, toggle}">
+                            <v-list-item-action>
+                                <v-checkbox></v-checkbox>
+                            </v-list-item-action>
+
+                            <v-list-item-content>
+                                <v-list-item-title>Equate I and L</v-list-item-title>
+                            </v-list-item-content>
+                            </template>
+                        </v-list-item>
+                        <v-list-item>
+                            <template v-slot:default="{active, toggle}">
+                            <v-list-item-action>
+                                <v-checkbox></v-checkbox>
+                            </v-list-item-action>
+
+                            <v-list-item-content>
+                                <v-list-item-title>Filter duplicate peptides</v-list-item-title>
+                            </v-list-item-content>
+                            </template>
+                        </v-list-item>
+                        <v-list-item>
+                            <template v-slot:default="{active, toggle}">
+                            <v-list-item-action>
+                                <v-checkbox></v-checkbox>
+                            </v-list-item-action>
+
+                            <v-list-item-content>
+                                <v-list-item-title>Advanced missing cleavage handling</v-list-item-title>
+                            </v-list-item-content>
+                            </template>
+                        </v-list-item>
+                    </v-list-group>
+
+                        <v-list-item link to='/settings'>
+                        <v-list-item-icon>
+                            <v-icon>mdi-settings</v-icon>
+                        </v-list-item-icon>
+                        <v-list-item-content>
+                            <v-list-item-title>Settings</v-list-item-title>
+                        </v-list-item-content>
+                    </v-list-item>
+                </v-list>
             </div>
-        </v-list-group>
-
-        <v-list-group prepend-icon="mdi-tune">
-        <template v-slot:activator>
-            <v-list-item-content>
-            <v-list-item-title>Search settings</v-list-item-title>
-            </v-list-item-content>
-        </template>
-
-        <v-list-item>
-            <template v-slot:default="{active, toggle}">
-            <v-list-item-action>
-                <v-checkbox></v-checkbox>
-            </v-list-item-action>
-
-            <v-list-item-content>
-                <v-list-item-title>Equate I and L</v-list-item-title>
-            </v-list-item-content>
-            </template>
-        </v-list-item>
-        <v-list-item>
-            <template v-slot:default="{active, toggle}">
-            <v-list-item-action>
-                <v-checkbox></v-checkbox>
-            </v-list-item-action>
-
-            <v-list-item-content>
-                <v-list-item-title>Filter duplicate peptides</v-list-item-title>
-            </v-list-item-content>
-            </template>
-        </v-list-item>
-        <v-list-item>
-            <template v-slot:default="{active, toggle}">
-            <v-list-item-action>
-                <v-checkbox></v-checkbox>
-            </v-list-item-action>
-
-            <v-list-item-content>
-                <v-list-item-title>Advanced missing cleavage handling</v-list-item-title>
-            </v-list-item-content>
-            </template>
-        </v-list-item>
-        </v-list-group>
-    </v-list>
-    <v-btn icon @click="isMini = !isMini" class="drawer-chevron">
-        <v-icon>{{ isMini ? 'mdi-chevron-left' : 'mdi-chevron-right' }}</v-icon>
-    </v-btn>
-    </v-navigation-drawer>
+        </v-navigation-drawer>
+        <div class="toolbar-content" :style="isMini ? 'display: none' : 'display: block;'">
+        </div>
+        <v-btn icon @click="isMini = !isMini" class="drawer-chevron" isMini :class="{'drawer-chevron-open': !isMini}">
+            <v-icon>{{ isMini ? 'mdi-chevron-right' : 'mdi-chevron-left' }}</v-icon>
+        </v-btn>
+    </div>
 </template>
 
 <script lang="ts">
@@ -120,10 +143,29 @@ export default class Toolbar extends Vue {
         position: absolute !important;
         top: 50%;
         transform: translateY(-50%);
+        z-index: 10;
+        left: 42px;
+    }
+
+    .drawer-chevron-open {
+        left: 218px !important;
     }
 
     .select-sample-button {
         margin: 0 auto;
         display: block !important;
+    }
+
+    .toolbar-content {
+        height: 100%;
+        position: relative; 
+        left: 80px; 
+        width: 176px; 
+        background-color: white;
+        border-right: 1px solid rgba(0, 0, 0, 0.12);
+    }
+
+    .toolbar-navigation-drawer > .v-navigation-drawer__border {
+        display: none;
     }
 </style>
