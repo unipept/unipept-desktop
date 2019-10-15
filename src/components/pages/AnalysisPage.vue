2a838c18f7929a097cfe09c6848b5414fe6ebc07
Pieter Verschaffelt <pieter.verschaffelt@ugent.be>
2019-10-15 09:23:26 +0200

    Analysis progress is now shown when sample is being analyzed

--- src/components/pages/AnalysisPage.vue
+++ src/components/pages/AnalysisPage.vue
@@ -2,15 +2,13 @@
     <v-container fluid>
         <v-row>
             <v-col>
-                <!-- TODO fix analysis in progress! -->
-                <single-dataset-visualizations-card :sample="this.$store.getters.activeDataset ? this.$store.getters.activeDataset.getDataset() : null" :analysisInProgress="false">
+                <single-dataset-visualizations-card :sample="this.$store.getters.activeDataset ? this.$store.getters.activeDataset.getDataset() : null" :analysisInProgress="$store.getters.datasetsInProgress > 0">
                 </single-dataset-visualizations-card>
             </v-col>
         </v-row>
         <v-row>
             <v-col>
-                <!-- TODO fix analysis in progress! -->
-                <functional-summary-card :sample="this.$store.getters.activeDataset ? this.$store.getters.activeDataset.getDataset() : null" :analysisInProgress="false">
+                <functional-summary-card :sample="this.$store.getters.activeDataset ? this.$store.getters.activeDataset.getDataset() : null" :analysisInProgress="$store.getters.datasetsInProgress > 0">
                 </functional-summary-card>
             </v-col>
         </v-row>
@@ -21,22 +19,17 @@
 import Vue from 'vue';
 import {Prop, Watch} from 'vue-property-decorator';
 import Component from 'vue-class-component';
-import AnalysisComponent from 'unipept-web-components/src/components/analysis/AnalysisComponent.vue';
 import PeptideContainer from 'unipept-web-components/src/logic/data-management/PeptideContainer';
 import SingleDatasetVisualizationsCard from 'unipept-web-components/src/components/visualizations/SingleDatasetVisualizationsCard.vue';
 import FunctionalSummaryCard from 'unipept-web-components/src/components/analysis/functional/FunctionalSummaryCard.vue';
 
 @Component({
     components: {
-        AnalysisComponent,
         SingleDatasetVisualizationsCard,
         FunctionalSummaryCard
     }
 })
 export default class AnalysisPage extends Vue {
-    private onSelectDataset(dataset: PeptideContainer): void {
-        this.$store.dispatch('selectDataset', dataset);
-    }
 }
 </script>
 
