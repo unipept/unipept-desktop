3d540ff6d87a042f3754f7b804a5b50cf5a9f174
Pieter Verschaffelt <pieter.verschaffelt@ugent.be>
2020-02-12 19:02:43 +0100

    Working on RecentProjects view

--- src/components/pages/HomePage.vue
+++ src/components/pages/HomePage.vue
@@ -2,9 +2,10 @@
     <v-container fluid>
         <v-row>
             <v-col>
-                <p>
-                    Welcome to Unipept Desktop! Please select a valid project folder or create a new project before continueing.
-                </p>
+                <!-- <p>
+                    Welcome to Unipept Desktop! Please select a valid project folder or create a new project before continuing.
+                </p> -->
+                <recent-projects></recent-projects>
             </v-col>
             <v-divider vertical></v-divider>
             <v-col>
@@ -12,19 +13,67 @@
                     <img src="@/assets/logo.svg" style="max-width: 200px;"/>
                     <span class="logo-headline">Unipept Desktop</span>
                     <span class="logo-subline">Version 0.0.1</span>
+
+                    <div class="project-actions">
+                        <div @click="createProject()">
+                            <v-icon>mdi-folder-plus-outline</v-icon>
+                            <span>Create new project</span>
+                        </div>
+                        <div>
+                            <v-icon>mdi-folder-download-outline</v-icon>
+                            <span>Import project</span>
+                        </div>
+                    </div>
                 </div>
             </v-col>
         </v-row>
+        <v-snackbar v-model="errorSnackbarVisible" bottom :timeout="0" color="error">
+            {{ errorMessage }}
+            <v-btn dark text @click="errorSnackbarVisible = false">Close</v-btn>
+        </v-snackbar>
     </v-container>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 import Component from "vue-class-component";
+import ProjectManager from "unipept-web-components/src/logic/data-management/project/ProjectManager";
+import Project from "unipept-web-components/src/logic/data-management/project/Project";
+import IOException from "unipept-web-components/src/logic/exceptions/IOException";
+import RecentProjects from "./../project/RecentProjects.vue";
+const { dialog } = require("electron").remote;
 
-@Component
+@Component({
+    components: {
+        RecentProjects
+    }
+})
 export default class HomePage extends Vue {
+    private errorMessage: string = "";
+    private errorSnackbarVisible: boolean = false;
 
+    private async createProject() {
+        const chosenPath: string | undefined = dialog.showOpenDialogSync({
+            properties: ["openDirectory"]
+        });
+        if (chosenPath) {
+            try {
+                const projectMng: ProjectManager = new ProjectManager();
+                const project: Project = await projectMng.initializeProject(chosenPath[0]);
+            } catch (err) {
+                if (err instanceof IOException) {
+                    this.showError(
+                        "Could not initialize your project. Please make sure that the chosen directory is writeable and try again."
+                    );
+                }
+            }
+        }
+    }
+
+    private showError(message: string) {
+        this.errorMessage = message;
+        this.errorSnackbarVisible = true;
+    }
 }
 </script>
 
@@ -38,5 +87,27 @@ export default class HomePage extends Vue {
     .logo-subline {
         font-size: 16px;
         color: #777;
+    }
+
+    .project-actions {
+        display: flex;
+        flex-direction: column;
+        margin-top: 16px;
+    }
+
+    .project-actions div {
+        display: flex;
+        align-items: center;
+        cursor: pointer;
+    }
+
+    .project-actions span {
+        font-size: 18px;
+        margin-left: 8px;
+    }
+
+    .project-actions .v-icon {
+        position: relative;
+        top: -2px;
     }
 </style>
---
+++ src/components/project/RecentProjects.vue
@@ -0,0 +1,58 @@
+<template>
+    <div>
+        <span>Recent projects</span>
+        <v-list two-line>
+            <v-list-item v-for="recentProject of recentProjects" :key="recentProject.name" @click="() => {}">
+                <v-list-item-content>
+                    <v-list-item-title>{{ recentProject.name }}</v-list-item-title>
+                    <v-list-item-subtitle>{{ recentProject.path }}</v-list-item-subtitle>
+                </v-list-item-content>
+                <v-list-item-action>
+                    <v-tooltip top>
+                        <template v-slot:activator="{ on }">
+                            <v-icon color="grey lighten-1" v-on="on">mdi-information</v-icon>
+                        </template>
+                        <span>Last opened on {{ recentProject.lastOpened }}</span>
+                    </v-tooltip>
+                </v-list-item-action>
+            </v-list-item>
+        </v-list>
+        <div class="open-project-button">
+            <v-icon>mdi-folder-open-outline</v-icon>
+            <a>Open other project...</a>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+
+@Component
+export default class RecentProjects extends Vue {
+    private recentProjects: { name: string, path: string, lastOpened: string }[] = [
+        {
+            name: "My Project",
+            path: "/usr/pverscha/Desktop/My Project",
+            lastOpened: "November 22, 2019"
+        },
+        {
+            name: "Test",
+            path: "/usr/pverscha/Documents/Unipept/Test",
+            lastOpened: "February 12, 2020"
+        }
+    ];
+}
+</script>
+
+<style>
+    .open-project-button {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .open-project-button a {
+        margin-left: 8px;
+    }
+</style>
