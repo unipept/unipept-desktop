83a5cb62925b011551b74e288d601b68c9a0f57b
Pieter Verschaffelt <pieter.verschaffelt@ugent.be>
2020-03-05 16:12:57 +0100

    Write script for deploying in GitHub Action

---
+++ scripts/deploy.js
@@ -0,0 +1,95 @@
+/**
+ * Automatically build and publish the desktop application to GitHub. This script should be run via a GitHub action.
+ * Script was derived from https://github.com/samuelmeuli/action-electron-builder/blob/master/index.js and adapted by
+ * Pieter Verschaffelt.
+ */
+
+const { execSync } = require("child_process");
+const { existsSync, readFileSync } = require("fs");
+const { join } = require("path");
+
+/**
+ * Logs to the console
+ */
+const log = msg => console.log(`\n${msg}`); // eslint-disable-line no-console
+
+/**
+ * Exits the current process with an error code and message
+ */
+const exit = msg => {
+	console.error(msg);
+	process.exit(1);
+};
+
+/**
+ * Executes the provided shell command and redirects stdout/stderr to the console
+ */
+const run = (cmd, cwd) => execSync(cmd, { encoding: "utf8", stdio: "inherit", cwd });
+
+/**
+ * Determines the current operating system (one of ["mac", "windows", "linux"])
+ */
+const getPlatform = () => {
+	switch (process.platform) {
+		case "darwin":
+			return "mac";
+		case "win32":
+			return "windows";
+		default:
+			return "linux";
+	}
+};
+
+/**
+ * Returns the value for an environment variable (or `null` if it's not defined)
+ */
+const getEnv = name => process.env[name.toUpperCase()] || null;
+
+/**
+ * Sets the specified env variable if the value isn't empty
+ */
+const setEnv = (name, value) => {
+	if (value) {
+		process.env[name.toUpperCase()] = value.toString();
+	}
+};
+
+/**
+ * Returns the value for an input variable (or `null` if it's not defined). If the variable is
+ * required and doesn't have a value, abort the action
+ */
+const getInput = (name, required) => {
+	const value = getEnv(`INPUT_${name}`);
+	if (required && !value) {
+		exit(`"${name}" input variable is not defined`);
+	}
+	return value;
+};
+
+/**
+ * Installs NPM dependencies and builds/releases the Electron app
+ */
+const runAction = () => {
+	const platform = getPlatform();
+	const pkgRoot = "./";
+	const pkgJsonPath = join(pkgRoot, "package.json");
+
+	// Make sure `package.json` file exists
+	if (!existsSync(pkgJsonPath)) {
+		exit(`\`package.json\` file not found at path "${pkgJsonPath}"`);
+	}
+
+	// Copy "github_token" input variable to "GH_TOKEN" env variable (required by `electron-builder`)
+	setEnv("GH_TOKEN", getInput("GITHUB_TOKEN"));
+
+	// Disable console advertisements during install phase
+    setEnv("ADBLOCK", true);
+    
+    run(`npm install`);
+
+	// Run NPM build script if it exists
+	log("Running the build script…");
+    run(`npm run electron:publish`, pkgRoot);
+};
+
+runAction();
