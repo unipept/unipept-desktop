ffd216e4d8e19cdf1a013a9b92131eb89741823e
Pieter Verschaffelt <pieter.verschaffelt@ugent.be>
2020-03-05 18:19:39 +0100

    GitHub action for automatically deploying app

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -17,8 +17,8 @@ const log = msg => console.log(`\n${msg}`); // eslint-disable-line no-console
  * Exits the current process with an error code and message
  */
 const exit = msg => {
-	console.error(msg);
-	process.exit(1);
+    console.error(msg);
+    process.exit(1);
 };
 
 /**
@@ -30,14 +30,14 @@ const run = (cmd, cwd) => execSync(cmd, { encoding: "utf8", stdio: "inherit", cw
  * Determines the current operating system (one of ["mac", "windows", "linux"])
  */
 const getPlatform = () => {
-	switch (process.platform) {
-		case "darwin":
-			return "mac";
-		case "win32":
-			return "windows";
-		default:
-			return "linux";
-	}
+    switch (process.platform) {
+    case "darwin":
+        return "mac";
+    case "win32":
+        return "windows";
+    default:
+        return "linux";
+    }
 };
 
 /**
@@ -49,9 +49,9 @@ const getEnv = name => process.env[name.toUpperCase()] || null;
  * Sets the specified env variable if the value isn't empty
  */
 const setEnv = (name, value) => {
-	if (value) {
-		process.env[name.toUpperCase()] = value.toString();
-	}
+    if (value) {
+        process.env[name.toUpperCase()] = value.toString();
+    }
 };
 
 /**
@@ -59,36 +59,36 @@ const setEnv = (name, value) => {
  * required and doesn't have a value, abort the action
  */
 const getInput = (name, required) => {
-	const value = getEnv(`INPUT_${name}`);
-	if (required && !value) {
-		exit(`"${name}" input variable is not defined`);
-	}
-	return value;
+    const value = getEnv(`INPUT_${name}`);
+    if (required && !value) {
+        exit(`"${name}" input variable is not defined`);
+    }
+    return value;
 };
 
 /**
  * Installs NPM dependencies and builds/releases the Electron app
  */
 const runAction = () => {
-	const platform = getPlatform();
-	const pkgRoot = "./";
-	const pkgJsonPath = join(pkgRoot, "package.json");
+    const platform = getPlatform();
+    const pkgRoot = "./";
+    const pkgJsonPath = join(pkgRoot, "package.json");
 
-	// Make sure `package.json` file exists
-	if (!existsSync(pkgJsonPath)) {
-		exit(`\`package.json\` file not found at path "${pkgJsonPath}"`);
-	}
+    // Make sure `package.json` file exists
+    if (!existsSync(pkgJsonPath)) {
+        exit(`\`package.json\` file not found at path "${pkgJsonPath}"`);
+    }
 
-	// Copy "github_token" input variable to "GH_TOKEN" env variable (required by `electron-builder`)
-	setEnv("GH_TOKEN", getInput("GITHUB_TOKEN"));
+    // Copy "github_token" input variable to "GH_TOKEN" env variable (required by `electron-builder`)
+    setEnv("GH_TOKEN", getInput("GITHUB_TOKEN"));
 
-	// Disable console advertisements during install phase
+    // Disable console advertisements during install phase
     setEnv("ADBLOCK", true);
-    
+
     run(`npm install`);
 
-	// Run NPM build script if it exists
-	log("Running the build script…");
+    // Run NPM build script if it exists
+    log("Running the build script…");
     run(`npm run electron:publish`, pkgRoot);
 };
 
