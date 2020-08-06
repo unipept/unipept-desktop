5889b4bec668cb1d606483093221c5e7c22f99c6
Pieter Verschaffelt <pieter.verschaffelt@ugent.be>
2020-08-06 10:54:43 +0200

    Fix linter errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,30 @@
 module.exports = {
-  root: true,
-  env: {
-    "node": true,
-    "browser": true
-  },
-  "extends": [
-    "plugin:vue/essential",
-    "eslint:recommended",
-    "@vue/typescript"
-  ],
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
-    "indent": ["error", 4],
-    "max-len": ["warn", {"code": 120}],
-    "object-curly-spacing": ["error", "always"],
-    "quotes": ["error", "double"],
-    "space-before-function-paren": ["error", "never"],
-    "no-inner-declarations": "off",
-    "brace-style": ["error", "1tbs"],
-    "keyword-spacing": "error",
-  },
-  parserOptions: {
-    "parser": "@typescript-eslint/parser",
-    "ecmaVersion": 6,
-  }
-}
+    root: true,
+    env: {
+        "node": true,
+        "browser": true,
+    },
+    "extends": [
+        "plugin:vue/essential",
+        "eslint:recommended",
+        "@vue/typescript",
+    ],
+    rules: {
+        "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+        "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+        "indent": ["error", 4],
+        "max-len": ["warn", { "code": 120 }],
+        "object-curly-spacing": ["error", "always"],
+        "quotes": ["error", "double"],
+        "space-before-function-paren": ["error", "never"],
+        "no-inner-declarations": "off",
+        "brace-style": ["error", "1tbs"],
+        "keyword-spacing": "error",
+        "no-async-promise-executor": "off",
+        "require-atomic-updates": "off",
+    },
+    parserOptions: {
+        "parser": "@typescript-eslint/parser",
+        "ecmaVersion": 6,
+    },
+};
