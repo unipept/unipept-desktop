50b1ab9bd99958fd0c82c44fba561cfb8d091b63
Pieter Verschaffelt <pieter.verschaffelt@ugent.be>
2020-10-16 13:38:06 +0200

    Add ability to start tests on Windows

--- test/src/Setup.js
+++ test/src/Setup.js
@@ -21,7 +21,18 @@ export default class Setup {
 
             return app.start();
         } else if (process.platform === "win32") {
+            let app = new Application({
+                path: path.join(
+                    __dirname,
+                    "..",
+                    "..",
+                    "dist_electron",
+                    "win-unpacked",
+                    "Unipept Desktop.exe"
+                )
+            });
 
+            return app.start();
         } else {
             throw Error("Current platform " + process.platform + " is not supported for the integration tests!");
         }
