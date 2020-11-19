b434f61dc3c028d34db07713249d57fd14a32034
Pieter Verschaffelt <pieter.verschaffelt@ugent.be>
2020-11-19 14:22:49 +0100

    Show update message if an update is available

--- src/logic/communication/github/GitHubCommunicator.ts
+++ src/logic/communication/github/GitHubCommunicator.ts
@@ -9,10 +9,22 @@ export default class GitHubCommunicator {
      * @return A list of all changes that were made during this release.
      * @throws Error if the given release could not be found, or if no internet connection is established.
      */
-    public async getReleaseNotes(appVersion: string): Promise<String> {
+    public async getReleaseNotes(appVersion: string): Promise<string> {
         const result = await NetworkUtils.getJSON(
             `https://api.github.com/repos/unipept/unipept-desktop/releases/tags/v${appVersion}`
         );
         return result.body;
+    }
+
+    public async getAllReleases(): Promise<string[]> {
+        const result = await NetworkUtils.getJSON(
+            "https://api.github.com/repos/unipept/unipept-desktop/releases"
+        );
+
+        return result.map((obj: any) => obj.tag_name).map((tag: string) => tag.replace("v", ""));
+    }
+
+    public async getMostRecentVersion(): Promise<string> {
+        return (await this.getAllReleases())[0];
     }
 }
