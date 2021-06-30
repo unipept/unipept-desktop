fdf565f93b998bae0536f21ccc3d8397d28459c2
Pieter Verschaffelt <pieter.verschaffelt@ugent.be>
2021-06-30 15:35:37 +0200

    Starting the container and filtering output works

--- src/logic/communication/docker/DockerCommunicator.ts
+++ src/logic/communication/docker/DockerCommunicator.ts
@@ -1,5 +1,6 @@
 import Dockerode, { DockerOptions } from "dockerode";
-import { NcbiId } from "unipept-web-components";
+import { NcbiId, ProgressListener } from "unipept-web-components";
+import ProgressInspectorStream from "@/logic/communication/docker/ProgressInspectorStream";
 
 
 export default class DockerCommunicator {
@@ -31,14 +32,50 @@ export default class DockerCommunicator {
      * @param databaseTypes The database types that correspond to the sources given in the first argument.
      * @param taxa A list of taxa ID's by which we should filter (these taxa, as well as their children in the lineage
      * tree will be included in the file).
-     *
+     * @param databaseFolder Folder in which the database under construction should be stored. This folder should be
+     * empty.
+     * @param indexFolder Folder in which the constructed database index structure should be kept.
+     * @param progressListener A callback function that can be used to
      */
     public async buildDatabase(
         databaseSources: string[],
         databaseTypes: string[],
         taxa: NcbiId[],
-        destinationFolder: string
+        databaseFolder: string,
+        indexFolder: string,
+        progressListener: (step: string, progress: number) => void
     ): Promise<void> {
+        if (!DockerCommunicator.connection) {
+            throw new Error("Connection to Docker daemon has not been initialized.");
+        }
 
+        DockerCommunicator.connection.run(
+            "pverscha/unipept-custom-db:1.1.1",
+            [],
+            new ProgressInspectorStream(progressListener),
+            {
+                Env: [
+                    "MYSQL_ROOT_PASSWORD=unipept",
+                    "MYSQL_DATABASE=unipept",
+                    "MYSQL_USER=unipept",
+                    "MYSQL_PASSWORD=unipept",
+                    `DB_TYPES=${databaseTypes.join(",")}`,
+                    `DB_SOURCES=${databaseSources.join(",")}`,
+                    `FILTER_TAXA=${taxa.join(",")}`
+                ],
+                PortBindings: {
+                    "3306/tcp": [{
+                        HostIP: "0.0.0.0",
+                        HostPort: "3306"
+                    }]
+                },
+                Binds: [
+                    // Mount the folder in which the MySQL-specific database files will be kept
+                    `${databaseFolder}:/var/lib/mysql`,
+                    // Mount the folder in which the reusable database index structure will be kept
+                    `${indexFolder}:/data`
+                ]
+            }
+        );
     }
 }
