8c74a694c62b4717f9eca7ed0fe3dfa635afdc18
Pieter Verschaffelt <pieter.verschaffelt@ugent.be>
2022-09-08 10:52:13 +0200

    Reference to Electron app was not properly imported

--- src/logic/application/ApplicationMigrationPreviousToV200alpha1.ts
+++ src/logic/application/ApplicationMigrationPreviousToV200alpha1.ts
@@ -1,5 +1,6 @@
 import ApplicationMigration from "@/logic/application/ApplicationMigration";
 import ConfigurationManager from "@/logic/configuration/ConfigurationManager";
+import { app } from "@electron/remote";
 
 /**
  * The default path of the application that's used for storing custom databases changed in version 2.0.0-alpha.1 of
@@ -8,7 +9,7 @@ import ConfigurationManager from "@/logic/configuration/ConfigurationManager";
 export default class ApplicationMigrationPreviousToV200alpha1 implements ApplicationMigration {
     public async upgrade(): Promise<void> {
         // Reset the configuration back to the default value.
-        const configManager = new ConfigurationManager();
+        const configManager = new ConfigurationManager(app);
         const defaultConfig = await configManager.getDefaultConfiguration();
         await configManager.writeConfiguration(defaultConfig);
     }
